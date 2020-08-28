2f26ed71f0c8ff033b67e347add0dab9e1e723be
jhidalgo990 <josehidalgo990@gmail.com>
2020-08-27 23:05:32 -0400

    skills section maqueted

--- js/intersectionObserver.js
+++ js/intersectionObserver.js
@@ -1,6 +1,8 @@
 const main = document.querySelector(".main");
+const skills = document.querySelector(".skills");
 const mainContainer = document.querySelector(".main-container");
 const sectionOne = document.querySelector(".sect-1");
+const sectionTwo = document.querySelector(".sect-2");
 
 const sectionOneOptions = {
   threshold: 0.9,
@@ -12,6 +14,7 @@ const sectionOneObserver = new IntersectionObserver(function (
 ) {
   entries.forEach((entry) => {
     if (!entry.isIntersecting) {
+      console.log("active");
       main.classList.add("active");
       mainContainer.classList.add("active");
     } else {
@@ -22,3 +25,28 @@ const sectionOneObserver = new IntersectionObserver(function (
 },
 sectionOneOptions);
 sectionOneObserver.observe(sectionOne);
+
+// Skills Section
+
+const sectionTwoOptions = {
+  threshold: 0.5,
+};
+
+const sectionTwoObserver = new IntersectionObserver(function (
+  entries,
+  sectionTwoObserver
+) {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      skills.classList.add("active");
+      console.log("section visible");
+      main.classList.add("active-2");
+    } else {
+      skills.classList.remove("active");
+      console.log("section hiden");
+      main.classList.remove("active-2");
+    }
+  });
+},
+sectionTwoOptions);
+sectionTwoObserver.observe(sectionTwo);
