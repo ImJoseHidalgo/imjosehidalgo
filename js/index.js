7e33d28bdd9d19ad776ea6263ed59278383c5075
José Hidalgo <josehidalgo990@gmail.com>
2020-11-06 01:01:16 -0400

    cursor pointer added

--- js/index.js
+++ js/index.js
@@ -32,3 +32,46 @@ document.querySelector(".menu-contact").addEventListener("click", () => {
 document.querySelector(".sect-1").addEventListener("click", () => {
   document.documentElement.classList.toggle("color");
 });
+
+/*  CURSOR POINTER
+    =============================== */
+let innerCursor = document.querySelector(".inner-cursor");
+
+document.addEventListener("mousemove", moveCursor);
+
+function moveCursor(e) {
+  let x = e.clientX;
+  let y = e.clientY;
+
+  innerCursor.style.left = `${x}px`;
+  innerCursor.style.top = `${y}px`;
+}
+
+const lang = document.querySelector(".blog-section");
+const lang2 = document.querySelector(".social-container");
+const lang3 = document.querySelector(".languages");
+const lang4 = document.querySelector(".logo");
+const lang5 = document.querySelector(".portf-container");
+const lang6 = document.querySelector("span");
+const lang7 = document.querySelector(".contact-footer-social");
+const lang8 = document.querySelector(".email");
+const lang9 = document.querySelector(".menu-email");
+
+mouseHover = function (e) {
+  e.addEventListener("mouseover", () => {
+    innerCursor.classList.add("grow");
+  });
+  e.addEventListener("mouseleave", () => {
+    innerCursor.classList.remove("grow");
+  });
+};
+
+mouseHover(lang);
+mouseHover(lang2);
+mouseHover(lang3);
+mouseHover(lang4);
+mouseHover(lang5);
+mouseHover(lang6);
+mouseHover(lang7);
+mouseHover(lang8);
+mouseHover(lang9);
